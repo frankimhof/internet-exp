5eacd2fe16d060463880128bf89e0812e469475e
frankimhof <imhof.frank@gmail.com>
2020-11-26 15:24:19 +0100

    correct path to certs, updated kex list

--- curl/curl_perf_kex.pl
+++ curl/curl_perf_kex.pl
@@ -5,17 +5,16 @@ use warnings;
 use Time::HiRes;
 
 if ($#ARGV + 1 != 2) {
-    print "\nUsage: experiment.pl server_common_name time_limit_in_seconds\n";
+    print "\nUsage: curl_perf_kex.pl <SERVER_COMMON_NAME> <TIME_LIMIT_IN_SECONDS>\n";
     exit;
 }
 
 my $server = $ARGV[0];
 my $timelimit = $ARGV[1];
-my $testresult_path = '/opt/test/testresults';
+my $testresult_path = '/opt/test/testresults/';
 
-#my @kex_algs = ('prime256v1', 'kyber512', 'kyber1024', 'p256_kyber512', 'p256_kyber1024', 'kyber512_90s', 'ntru_hps2048509', 'lightsaber', 'p256_kyber512_90s', 'p256_lightsaber');
-my @kex_algs = ('prime256v1');
-
+my @kex_algs = ('prime256v1', 'kyber512', 'kyber1024', 'p256_kyber512', 'saber', 'lightsaber', 'firesaber', 'ntru_hps2048509', 'ntru_hps2048677', 'ntru_hps4096821');
+#my @kex_algs = ('prime256v1', 'p256_kyber90s512', 'p256_sikep434', 'p256_frodo640aes');
 my @files = ('index1kb.html', 'index10kb.html', 'index100kb.html', 'index1000kb.html');
 
 foreach my $kex_alg (@kex_algs) {
@@ -23,20 +22,20 @@ foreach my $kex_alg (@kex_algs) {
     my $output = $testresult_path . 'curl-kex-' . $kex_alg . '-' . $sig_alg;
     foreach my $file (@files) {
         my $port = '4433';
-        system("printf '$file' >> $output.txt");
-        my $check_curl_command = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert certs/ecdsap256_CA.crt -H 'Connection: close'";
+        system("printf '$file' >> $output.csv");
+        my $check_curl_command = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert /certs/ecdsap256_CA.crt -H 'Connection: close'";
         system($check_curl_command . "2> /dev/null");
         if($? != 0){
                 printf("Curl exited with code: %d", $?>>8);
                 last;
         }
-        my $cmd = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert certs/ecdsap256_CA.crt -H 'Connection: close' -w '\,%{time_total}' 2> /dev/null | tail -n1";
+        my $cmd = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert /certs/ecdsap256_CA.crt -H 'Connection: close' -w '\,%{time_total}' 2> /dev/null | tail -n1";
         system("echo $cmd");
         my $endtime = Time::HiRes::time()+$timelimit;
         while(Time::HiRes::time()<$endtime){
-            system($cmd . ">> $output.txt");
+            system($cmd . ">> $output.csv");
         }
-        system("printf '\n' >> $output.txt");
+        system("printf '\n' >> $output.csv");
     }
 }
 
