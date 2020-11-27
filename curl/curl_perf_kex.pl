69526396826d6e1350aefe4b6a08c78279f59b32
frankimhof <imhof.frank@gmail.com>
2020-11-28 00:35:49 +0100

    enabling round 3 candidates

--- curl/curl_perf_kex.pl
+++ curl/curl_perf_kex.pl
@@ -26,8 +26,8 @@ my $packet_loss=`cat out.txt | tail -1 | tr -d '\n'`;
 system("echo $packet_loss");
 
 
-#my @kex_algs = ('prime256v1', 'kyber512', 'kyber1024', 'p256_kyber512', 'saber', 'lightsaber', 'firesaber', 'ntru_hps2048509', 'ntru_hps2048677', 'ntru_hps4096821');
-my @kex_algs = ('prime256v1', 'p256_kyber90s512', 'p256_sikep434', 'p256_frodo640aes');
+my @kex_algs = ('prime256v1', 'kyber512', 'kyber1024', 'p256_kyber512', 'saber', 'lightsaber', 'firesaber', 'ntru_hps2048509', 'ntru_hps2048677', 'ntru_hps4096821');
+#my @kex_algs = ('prime256v1', 'p256_kyber90s512', 'p256_sikep434', 'p256_frodo640aes');
 my @files = ('index1kb.html', 'index10kb.html', 'index100kb.html', 'index1000kb.html');
 
 foreach my $kex_alg (@kex_algs) {
