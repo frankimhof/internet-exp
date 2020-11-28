44b0dde13806530b6cbcd166c16c1177d52adb2d
frankimhof <imhof.frank@gmail.com>
2020-11-28 13:22:57 +0100

    ping testserver (not google) and print output nicely

--- curl/curl_perf_kex.pl
+++ curl/curl_perf_kex.pl
@@ -14,19 +14,25 @@ my $timelimit = $ARGV[1];
 my $testresult_path = '/opt/test/testresults/';
 
 # Get RTT and Packet-Loss information via Ping and store it for naming files later
-system("echo Testing connection for RTT and Packet Loss...");
-system("printf RTT_ >> out.txt");
-system("ping google.com -c 10 | tail -1 | awk '{print \$4}' | cut -d '/' -f 2 >> out.txt");
+system("echo =================================================================");
+system("echo Testing connection with server: $server");
+system("echo -----------------------------------------------------------------");
+system("printf RTT_ > out.txt");
+system("ping '$server' -c 10 | tail -1 | awk '{print \$4}' | cut -d '/' -f 2 >> out.txt");
 system("printf PKTLOSS_ >> out.txt");
-system("ping google.com -c 10 | tail -2 | head -n1 | awk '{print \$7}' >> out.txt");
+system("ping '$server' -c 10 | tail -2 | head -n1 | awk '{print \$7}' >> out.txt");
 
 my $rtt=`cat out.txt | head -n1 | tr -d '\n'`;
-system("echo $rtt");
 my $packet_loss=`cat out.txt | tail -1 | tr -d '\n'`;
-system("echo $packet_loss");
-
-
-my @kex_algs = ('prime256v1', 'kyber512', 'kyber1024', 'p256_kyber512', 'saber', 'lightsaber', 'firesaber', 'ntru_hps2048509', 'ntru_hps2048677', 'ntru_hps4096821');
+system("echo RTT '\(average\)'     = $rtt ms");
+system("echo Packet Loss           = '$packet_loss'");
+system("echo ");
+sleep 3;
+system("echo =================================================================");
+system("echo Running test curl_perf_kex");
+system("echo -----------------------------------------------------------------");
+sleep 2;
+my @kex_algs = ('prime256v1', 'kyber512');#, 'kyber1024', 'p256_kyber512', 'saber', 'lightsaber', 'firesaber', 'ntru_hps2048509', 'ntru_hps2048677', 'ntru_hps4096821');
 #my @kex_algs = ('prime256v1', 'p256_kyber90s512', 'p256_sikep434', 'p256_frodo640aes');
 my @files = ('index1kb.html', 'index10kb.html', 'index100kb.html', 'index1000kb.html');
 
@@ -40,7 +46,9 @@ foreach my $kex_alg (@kex_algs) {
         my $check_curl_command = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert /certs/ecdsap256_CA.crt -H 'Connection: close'";
         system($check_curl_command . "2> /dev/null");
         if($? != 0){
-                printf("Curl exited with code: %d", $?>>8);
+                system("echo =================================================================");
+                system("echo =================================================================");
+                printf("ERROR: Curl exited with code: %d", $?>>8);
                 last;
         }
         my $cmd = "curl -s 'https://$server:4433/$file' --curves $kex_alg --cacert /certs/ecdsap256_CA.crt -H 'Connection: close' -w '\,%{time_total}' 2> /dev/null | tail -n1";
@@ -51,5 +59,10 @@ foreach my $kex_alg (@kex_algs) {
         }
         system("printf '\n' >> $output.csv");
     }
+    system("echo -----------------------------------------------------------------");
 }
 
+system("echo ");
+system("echo =================================================================");
+system("echo =================================================================");
+system("echo DONE. Testresults have been written to /opt/test/testresults");
